4f96644a03e0edddbf2f8cdf997b26b73edecd59
Darlan Alves <me@darlanalv.es>
2023-07-27 12:23:20 +0200

    chore: unset module after installing it

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ cd /home/node/app
 if [ -n "$FN_MODULE" ]; then
   npm i -S fn$FN_MODULE;
   export FN_PATH="/home/node/app/node_modules/fn/index.js";
+  unset FN_MODULE
 fi
 
 node /home/node/lambda.mjs
