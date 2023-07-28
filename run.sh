3dbeefc2bc328cc0217b4d93e320d514f229bcbb
Darlan Alves <me@darlanalv.es>
2023-07-28 10:44:34 +0200

    fix: install fn module in the right folder

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -e
 
 echo "Running fn from $FN_MODULE or $FN_PATH"
 cd /home/node/app
-[ -n "$FN_MODULE" ] && npm i -S $FN_MODULE;
+[ -n "$FN_MODULE" ] && (cd /home/node && npm i -S $FN_MODULE);
 
 node /home/node/lambda.mjs
